73a43936326c4505bd7a4bffad9c9efeb15d1239
davidemioma <davidemiomauche@gmail.com>
2022-04-21 15:32:19 +0100

    page

--- src/components/filters/DesktopFilters.js
+++ src/components/filters/DesktopFilters.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import classes from "./DesktopFilters.module.css";
 
 function DesktopFilters(props) {
+  const [active, setActive] = useState(0);
+
   const onClickHandler = (e) => {
     if (e.target.dataset.value) {
       props.onTargetValue(e.target.dataset.value);
@@ -9,21 +12,33 @@ function DesktopFilters(props) {
 
   return (
     <div onClick={onClickHandler} className={classes.filters}>
-      <div data-value="overview">
+      <div
+        className={active === 0 && classes.active}
+        data-value="overview"
+        onClick={() => setActive(0)}
+      >
         <p>
           <span>01</span>
           overview
         </p>
       </div>
 
-      <div data-value="structure">
+      <div
+        className={active === 1 && classes.active}
+        data-value="structure"
+        onClick={() => setActive(1)}
+      >
         <p>
           <span>02</span>
           structure
         </p>
       </div>
 
-      <div data-value="surface">
+      <div
+        className={active === 2 && classes.active}
+        data-value="surface"
+        onClick={() => setActive(2)}
+      >
         <p>
           <span>03</span>
           surface
--- src/components/filters/MobileFilters.js
+++ src/components/filters/MobileFilters.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import classes from "./MobileFilters.module.css";
 
 function MobileFilters(props) {
+  const [active, setActive] = useState(0);
+
   const onClickHandler = (e) => {
     if (e.target.dataset.value) {
       props.onTargetValue(e.target.dataset.value);
@@ -9,11 +12,29 @@ function MobileFilters(props) {
 
   return (
     <div onClick={onClickHandler} className={classes.filters}>
-      <span data-value="overview">overview</span>
+      <span
+        className={active === 0 && classes.active}
+        data-value="overview"
+        onClick={() => setActive(0)}
+      >
+        overview
+      </span>
 
-      <span data-value="structure">structure</span>
+      <span
+        className={active === 1 && classes.active}
+        data-value="structure"
+        onClick={() => setActive(1)}
+      >
+        structure
+      </span>
 
-      <span data-value="surface">surface</span>
+      <span
+        className={active === 2 && classes.active}
+        data-value="surface"
+        onClick={() => setActive(2)}
+      >
+        surface
+      </span>
     </div>
   );
 }
